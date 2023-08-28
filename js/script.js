3a3c6b5a96383b4a83c8eaba6dfc7e3efde77898
Yasaman <125014861+YasamanMirdoraghi@users.noreply.github.com>
2023-08-28 14:45:01 +0330

    developed by :YasamanMirdoraghi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿
+//random background
+let redValue, greenValue, blueValue
+let RandomColor = document.querySelector(".title")
+
+setInterval(function () {
+    redValue = Math.floor(Math.random() * 256)
+    greenValue = Math.floor(Math.random() * 256)
+    blueValue = Math.floor(Math.random() * 256)
+
+    RandomColor.innerHTML="Random Background"+
+    'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ');'
+
+    document.body.style.backgroundColor = 
+ 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')'
+    
+}, 700)
+
